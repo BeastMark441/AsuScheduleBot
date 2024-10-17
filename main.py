e5ba6f50f23a90f5730d45beced62e4b7cbf9c98
Leonid <48765566+DiFFoZ@users.noreply.github.com>
2024-10-18 05:51:01 +0700

    Setup logging to the file

--- main.py
+++ main.py
@@ -5,16 +5,18 @@ from dotenv import load_dotenv
 from telegrambot import TelegramBot
 
 def setup_logging() -> None:
-
     level: logging._Level = logging.INFO
     if os.getenv("DEV") == 'True':
         level = logging.DEBUG
 
     logging.basicConfig(
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        format='[%(asctime)s %(levelname)s][%(name)s] %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        filename='latest.log', encoding='utf-8',
         level=level
     )
 
+    # removes spam
     logging.getLogger("httpx").setLevel(logging.WARNING)
 
 def main() -> None:
