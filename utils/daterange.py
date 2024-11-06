3edf604efb15fdb1616c1f3e4ee587e3232e369c
Leonid <48765566+DiFFoZ@users.noreply.github.com>
2024-11-07 05:41:27 +0700

    Remove optional typing in datarange

--- utils/daterange.py
+++ utils/daterange.py
@@ -1,12 +1,11 @@
 from dataclasses import dataclass
 from datetime import date, datetime
-from typing import Optional
 
 @dataclass
 class DateRange:
     # range of time
     start_date: date
-    end_date: Optional[date] = None
+    end_date: date | None = None
 
     def __post_init__(self) -> None:
         if isinstance(self.start_date, datetime):
