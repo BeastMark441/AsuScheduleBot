335a87d6b260aa80fe99b4d98c8949d98cbf4ad8
Leonid <48765566+DiFFoZ@users.noreply.github.com>
2024-11-08 13:55:14 +0700

    Fix no error log sent to the devs

--- telegrambot/bot.py
+++ telegrambot/bot.py
@@ -4,18 +4,15 @@ import logging
 import traceback
 from telegram.ext import Application, CommandHandler, ContextTypes
 from telegram.constants import ParseMode
-from telegram import Update  # Добавляем импорт Update
 
 from .commands import *
 
 class TelegramBot():
-    developer_chat_id: int = 0
-    
     def __init__(self, token: str, dev_chat_id: int):
         if not token:
             raise ValueError("Токен бота не может быть пустым")
         self.token: str = token
-        self.developer_chat_id = dev_chat_id
+        self.developer_chat_id: int = dev_chat_id
 
     def run(self):
         application = (
@@ -33,9 +30,9 @@ class TelegramBot():
         application.add_handler(CommandHandler("start", start_callback))
         application.add_handler(CommandHandler("cleansavegroup", cleansavegroup_callback))
         application.add_handler(CommandHandler("cleansavelect", cleansavelect_callback))
-
-        if self.developer_chat_id != 0:
-            application.add_error_handler(error_handler)
+        
+        application.bot_data['DEVELOPER_CHAT_ID'] = self.developer_chat_id
+        application.add_error_handler(error_handler)
 
         application.run_polling(allowed_updates=Update.ALL_TYPES)
 
@@ -45,24 +42,18 @@ async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> N
     # Log the error before we do anything else, so we can see it even if something breaks.
     logging.error("Exception while handling an update:", exc_info=context.error)
 
-    # traceback.format_exception returns the usual python message about an exception, but as a
-    # list of strings rather than a single string, so we have to join them together.
-    tb_list = traceback.format_exception(None, context.error, context.error.__traceback__) # pyright: ignore
-    tb_string = "".join(tb_list)
-
     # Build the message with some markup and additional information about what happened.
     # You might need to add some logic to deal with messages longer than the 4096 character limit.
     update_str = update.to_dict() if isinstance(update, Update) else str(update)
     message = (
-        "An exception was raised while handling an update\n"
+        "An exception was raised while handling an update\nSee latest.log to get trace back\n"
         f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}"
         "</pre>\n\n"
-        f"<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n"
-        f"<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n"
-        f"<pre>{html.escape(tb_string)}</pre>"
+        f"<pre>chat_data = {html.escape(str(context.chat_data))}</pre>\n\n"
+        f"<pre>user_data = {html.escape(str(context.user_data))}</pre>\n\n"
     )
 
     # Finally, send the message
-    _ = await context.bot.send_message(
-        chat_id=TelegramBot.developer_chat_id, text=message, parse_mode=ParseMode.HTML
+    await context.bot.send_message(
+        chat_id=context.application.bot_data['DEVELOPER_CHAT_ID'], text=message, parse_mode=ParseMode.HTML
     )
